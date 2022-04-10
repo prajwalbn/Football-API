047472a7873b60bdcbd95d72578f84328abdcd29
prajwalbn <prajwalbn8@gmail.com>
2022-04-10 11:55:36 +0100

    updated security.py file

--- security.py
+++ security.py
@@ -1,13 +1,13 @@
 from hmac import compare_digest
-from models.user import UserModel
+from models.manager import ManagerModel
 
 
 def authenticate(username, password):
-    user = UserModel.find_by_username(username)
+    user = ManagerModel.find_by_username(username)
     if user and compare_digest(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return UserModel.find_by_id(user_id)
+    return ManagerModel.find_by_id(user_id)
